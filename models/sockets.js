d360fc0d151210964016f42bf99624af9bd31ce1
Rodrigo Lopez <rodrigo_l321@hotmail.com>
2022-05-27 21:32:52 -0500

    Primer commit

---
+++ models/sockets.js
@@ -0,0 +1,21 @@
+
+class Sockets{
+
+    constructor( io ){
+        this.io = io;
+        this.socketsEvents();
+    }
+
+    socketsEvents(){
+        //En conexion
+        this.io.on('connection', (socket) => {
+            //Escuchar evento
+            socket.on('mensaje-to-server', (data) => {
+                console.log( data )
+                this.io.emit('mensaje-from-server', data);
+            });
+        });
+    }
+}
+
+module.exports = Sockets;
